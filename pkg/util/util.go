ecf3c5391b05f5d1dc3746f9099f8892faf4bde5
Alexander Lyon <alexander.lyon@codethink.co.uk>
2019-06-18 13:56:57 +0100

    Document ToBytes function

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -2,7 +2,8 @@ package util
 
 import "golang.org/x/crypto/ed25519"
 
-func ToBytes(key ed25519.PublicKey) [32]byte {
+// converts a public key to a fixed-size byte array
+func ToBytes(key ed25519.PublicKey) [ed25519.PublicKeySize]byte {
 	var bytes [ed25519.PublicKeySize]byte
 	copy(bytes[:], key)
 	return bytes
